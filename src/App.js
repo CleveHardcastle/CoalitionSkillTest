5033a4c276b30c9433d425b4899f4a262cdafcfa
CleveHardcastle <treyhardcastle@gmail.com>
2023-02-10 16:48:49 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <p className="text-4xl">Hello!</p>
+    </div>
+  );
+}
+
+export default Home;
